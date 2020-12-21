a55fd821e3aae036f40ad4906ee2b91486e97a43
jinczing <jamesc890116@gmail.com>
2020-12-21 09:44:43 +0000

    update multihead_trainer

--- model.py
+++ model.py
@@ -45,7 +45,7 @@ class MyModel(nn.Module):
 
     self.intermid = nn.ModuleList(intermid)
     torch.nn.init.kaiming_normal_(self.intermid[2].weight)
-    self.arc_face = ArcMarginProduct(512, self.classes_number)
+    self.arc_face = Arcface(512, self.classes_number)
     self.head = nn.Linear(512, self.classes_number, bias=False)
 
 
@@ -87,10 +87,10 @@ class MultiHeadModel(nn.Module):
     self.intermid_unique = nn.ModuleList(intermid)
     torch.nn.init.kaiming_normal_(self.intermid_unique[2].weight)
     
-    self.arc_face_root = ArcMarginProduct(512, 168)
-    self.arc_face_consonant = ArcMarginProduct(512, 11)
-    self.arc_face_vowel = ArcMarginProduct(512, 18)
-    self.arc_face_unique = ArcMarginProduct(512, 1295)
+    self.arc_face_root = Arcface(512, 168)
+    self.arc_face_consonant = Arcface(512, 11)
+    self.arc_face_vowel = Arcface(512, 18)
+    self.arc_face_unique = Arcface(512, 1295)
 
     self.head_root = nn.Linear(512, 168, bias=False)
     self.head_consonant = nn.Linear(512, 11, bias=False)
@@ -136,47 +136,91 @@ class MultiHeadModel(nn.Module):
 
     return multi_head_outputs
 
+def l2_norm(input,axis=1):
+    norm = torch.norm(input,2,axis,True)
+    output = torch.div(input, norm)
+    return output
 
-class ArcMarginProduct(nn.Module):
-    r"""Implement of large margin arc distance: :
-        Args:
-            in_features: size of each input sample
-            out_features: size of each output sample
-            s: norm of input feature
-            m: margin
-            cos(theta + m)
-        """
-    def __init__(self, in_features, out_features, s=30.0, m=0.35, easy_margin=False):
-        super(ArcMarginProduct, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.s = s
-        self.m = m
-        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
-        nn.init.xavier_uniform_(self.weight)
-
-        self.easy_margin = easy_margin
+class Arcface(nn.Module):
+    # implementation of additive margin softmax loss in https://arxiv.org/abs/1801.05599    
+    def __init__(self, embedding_size=512, classnum=51332,  s=30., m=0.35):
+        super(Arcface, self).__init__()
+        self.classnum = classnum
+        self.weight = Parameter(torch.Tensor(embedding_size,classnum))
+        # initial kernel
+        self.weight.data.uniform_(-1, 1).renorm_(2,1,1e-5).mul_(1e5)
+        self.m = m # the margin value, default is 0.5
+        self.s = s # scalar value default is 64, see normface https://arxiv.org/abs/1704.06369
         self.cos_m = math.cos(m)
         self.sin_m = math.sin(m)
-        self.th = math.cos(math.pi - m)
-        self.mm = math.sin(math.pi - m) * m
-
-    def forward(self, input, label):
-        # --------------------------- cos(theta) & phi(theta) ---------------------------
-        cosine = F.linear(F.normalize(input), F.normalize(self.weight))
-        sine = torch.sqrt((1.0 - torch.pow(cosine, 2)).clamp(0, 1))
-        phi = cosine * self.cos_m - sine * self.sin_m
-        if self.easy_margin:
-            phi = torch.where(cosine > 0, phi, cosine)
-        else:
-            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
-        # --------------------------- convert label to one-hot ---------------------------
-        # one_hot = torch.zeros(cosine.size(), requires_grad=True, device='cuda')
-        one_hot = torch.zeros(cosine.size(), device='cuda')
-        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
-        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
-        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
-        output *= self.s
-        # print(output)
-
+        self.mm = self.sin_m * m  # issue 1
+        self.threshold = math.cos(math.pi - m)
+    def forward(self, embbedings, label):
+        # weights norm
+        nB = len(embbedings)
+        kernel_norm = l2_norm(self.weight,axis=0)
+        # cos(theta+m)
+        cos_theta = torch.mm(embbedings,kernel_norm)
+#         output = torch.mm(embbedings,kernel_norm)
+        cos_theta = cos_theta.clamp(-1,1) # for numerical stability
+        cos_theta_2 = torch.pow(cos_theta, 2)
+        sin_theta_2 = 1 - cos_theta_2
+        sin_theta = torch.sqrt(sin_theta_2)
+        cos_theta_m = (cos_theta * self.cos_m - sin_theta * self.sin_m)
+        # this condition controls the theta+m should in range [0, pi]
+        #      0<=theta+m<=pi
+        #     -m<=theta<=pi-m
+        cond_v = cos_theta - self.threshold
+        cond_mask = cond_v <= 0
+        keep_val = (cos_theta - self.mm) # when theta not in [0,pi], use cosface instead
+        cos_theta_m[cond_mask] = keep_val[cond_mask]
+        output = cos_theta * 1.0 # a little bit hacky way to prevent in_place operation on cos_theta
+        idx_ = torch.arange(0, nB, dtype=torch.long)
+        output[idx_, label] = cos_theta_m[idx_, label]
+        output *= self.s # scale up in order to make softmax work, first introduced in normface
         return output
+
+
+# class Arcface(nn.Module):
+#     r"""Implement of large margin arc distance: :
+#         Args:
+#             in_features: size of each input sample
+#             out_features: size of each output sample
+#             s: norm of input feature
+#             m: margin
+#             cos(theta + m)
+#         """
+#     def __init__(self, in_features, out_features, s=30.0, m=0.35, easy_margin=False):
+#         super(ArcMarginProduct, self).__init__()
+#         self.in_features = in_features
+#         self.out_features = out_features
+#         self.s = s
+#         self.m = m
+#         self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+#         nn.init.xavier_uniform_(self.weight)
+
+#         self.easy_margin = easy_margin
+#         self.cos_m = math.cos(m)
+#         self.sin_m = math.sin(m)
+#         self.th = math.cos(math.pi - m)
+#         self.mm = math.sin(math.pi - m) * m
+
+#     def forward(self, input, label):
+#         # --------------------------- cos(theta) & phi(theta) ---------------------------
+#         cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+#         sine = torch.sqrt((1.0 - torch.pow(cosine, 2)).clamp(0, 1))
+#         phi = cosine * self.cos_m - sine * self.sin_m
+#         if self.easy_margin:
+#             phi = torch.where(cosine > 0, phi, cosine)
+#         else:
+#             phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+#         # --------------------------- convert label to one-hot ---------------------------
+#         # one_hot = torch.zeros(cosine.size(), requires_grad=True, device='cuda')
+#         one_hot = torch.zeros(cosine.size(), device='cuda')
+#         one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+#         # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
+#         output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
+#         output *= self.s
+#         # print(output)
+
+#         return output
